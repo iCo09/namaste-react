6e458b0a9efaca449e73498651c5d124205fb75c
Rahul Bisht <bishtrahul6400@gmail.com>
2025-02-01 15:13:04 +0530

    Functional component and JSX

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
-const heading = React.createElement("h1", {id:"heading"}, "Hi from ReactJs and Akshay");
+import ReactDOM from "react-dom/client";
+
+//JSX (transpiled before it reaches to JS engine so react can understand) => transpiling is done by parcel => babel
+
+//JSX -> React.creatElement -> ReactElement-JS object -> HTMLElement(render)
+
+const Title = () => (
+<h1 id="heading">
+    Namaste from JSX🚀
+    </h1>
+);
+
+const data = 1000;
+ 
+//React Functional Component -> A normal JS function which returns JSX code
+const HeadingComponent = () => (
+    <div id="container"> 
+    <Title/>
+    {Title()}
+    <h1>Namaste React Functional Component🚀</h1>
+    </div>  
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-  
+//root.render(jsxHeading);
+root.render(<HeadingComponent/>)
+
