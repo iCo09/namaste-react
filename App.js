16d8e91dc86e4ad6583fd1d8c65c92fd20b6ea2e
Rahul Bisht <bishtrahul6400@gmail.com>
2025-02-04 00:11:11 +0530

    Food App

--- App.js
+++ App.js
@@ -1,29 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX (transpiled before it reaches to JS engine so react can understand) => transpiling is done by parcel => babel
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-conatiner">
+            <img className="logo" src="https://imgs.search.brave.com/NaO2djcf-A8i93pzwbpBRjUOoivZGoZ6KLE_jlueB3I/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nb2Rlc2lnbi5u/ZXQvbG9nby9zbW9r/aW5nLWJ1cmdlci13/aXRoLWxldHR1Y2Ut/MzYyNGxkLnBuZz9u/d209MSZud3M9MSZp/bmR1c3RyeT1mb29k/JnNmPSZ0eHRfa2V5/d29yZD1BbGw"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
 
-//JSX -> React.creatElement -> ReactElement-JS object -> HTMLElement(render)
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+            <img className="res-logo" src="https://tse1.mm.bing.net/th?id=OIP.sBFDF3XzKrprpSea0yGY7wHaE7&pid=Api&P=0&h=180" />
+            <h3>Meghana Foods</h3>
+            <h4>
+                Biryani, North Indian, Asian
+            </h4>
+            <h4>
+                4.4 stars
+            </h4>
+            <h4>
+                38 minutes
+            </h4>
+        </div>
+    )
+}
 
-const Title = () => (
-<h1 id="heading">
-    Namaste from JSX🚀
-    </h1>
-);
+const Body = () => {
+    return (
+        <div className="body" >
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
 
-const data = 1000;
- 
-//React Functional Component -> A normal JS function which returns JSX code
-const HeadingComponent = () => (
-    <div id="container"> 
-    <Title/>
-    {Title()}
-    <h1>Namaste React Functional Component🚀</h1>
-    </div>  
-);
+const AppLayout = () => {
+    return (
+        <div className="app" >
+            <Header />
+            <Body/>
+        </div>
+    )
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//root.render(jsxHeading);
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
 
