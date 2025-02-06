5547b9165712262a639b1c91730dbdd384ac1c34
Rahul Bisht <bishtrahul6400@gmail.com>
2025-02-07 00:44:24 +0530

    useEffect and Shimmer effect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-
     //State Variable -> HOOKS
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
 
-    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
-  
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        // const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&page_type=DESKTOP_WEB_LISTING");
+        // const json = await data.json();
+        // console.log(json);
+        const timer = setTimeout(() => {
+            setListOfRestaurant(resList); // Load resList after a delay
+        }, 1000); // 2-second delay to simulate an API call
+
+        return () => clearTimeout(timer); // Cleanup timer on component unmount
+    };
+
+    if(listOfRestaurant.length === 0){
+        return <Shimmer/>
+    }
+ 
     return (
         <div className="body" >
             <div className="filter">
