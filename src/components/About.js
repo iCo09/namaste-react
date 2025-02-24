d17978c1494396c09d74be5a7eb8f1bcde2bc90c
Rahul Bisht <bishtrahul6400@gmail.com>
2025-02-24 23:14:34 +0530

    class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,20 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React Web Series</h2>
-        </div>
-    )
-};
+import User from "./User";
+import UserClass from "./UserClass";
+
+class About extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is Namaste React Web Series</h2>
+                <User name={"Akshay Saini by function"}/>
+            </div>
+        )
+    }
+}
+
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,26 @@
+import React from "react";
+class UserClass extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+  render() {
+    return(
+        <div className="user-card" >
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={() => {
+            this.setState({
+                count: this.state.count + 1
+            })
+        }}>Count Increase</button>
+        <h2>Name: {this.props.name}</h2>
+        <h3>Location: Dehradun</h3>
+        <h4>Contact: @akshaymarch7</h4>
+    </div>
+    );
+  }
+}
+
+export default UserClass;
